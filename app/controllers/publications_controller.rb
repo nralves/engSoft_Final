49beb188ca0f1658d55edfeb0687e4e9c12fe027
Nuno Alves <nr.alves@campus.fct.unl.pt>
2014-12-09 23:55:00 +0000

    Questions new and Publications new with checkbox skills done

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -85,6 +85,6 @@ class PublicationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def publication_params
-      params.require(:publication).permit(:name, :body, :user_id)
+      params.require(:publication).permit(:name, :body, :user_id, { skill_ids:[] })
     end
 end
