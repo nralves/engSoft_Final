bb8ee440b01990486ffe0341a6e0e58bfd7acec8
Nuno Alves <nr.alves@campus.fct.unl.pt>
2014-12-09 23:38:07 +0000

    Checkboxes on question creation for skills

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -99,7 +99,7 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:title, :body, :user_id)
+      params.require(:question).permit(:title, :body, :user_id, { skill_ids:[] })
     end
 
     def answer_params
