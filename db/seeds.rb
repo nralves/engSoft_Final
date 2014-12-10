480b2fe8d2397a3f135fa0a6cf2ef09ea132cd6f
Nuno Alves <nr.alves@campus.fct.unl.pt>
2014-12-10 12:41:37 +0000

    Seed modificado

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,10 +32,41 @@ Job.create(title: "Software Engineering Junior", body:"Requere-se um recem licen
 Job.create(title: "RoR Develpment", body:"Requere-se um recem licenciado para trabalhar como Ruby on Rails Developer na nosa empresa a full-time.", author:"OpenWeb", available: true)
 Job.create(title: "Computer Networks Manager", body:"Requere-se um recem licenciado para trabalhar como Computer Network Manager Junior na nosa empresa a full-time.", author:"OpenNetwork", available: false)
 
-Publication.create(name: "Enunciado de CI", body: "Este é o enunciado de Codificação de Informação",user_id: 1, :document => File.open(File.join(Rails.root, '/public/system/publications/pdf/tea.pdf')) )
+Publication.create(name: "Enunciado de CI", body: "Este é o enunciado de Codificação de Informação",user_id: 2, :document => File.open(File.join(Rails.root, '/public/system/publications/pdf/tea.pdf')) )
 Publication.create(name: "Static Typing for Ruby on Rails", body: "Ruby on Rails (or just “Rails”) is a popular web application framework built on top of Ruby, an object-oriented scripting language. While Ruby’s powerful features help make Rails development extremely lightweight, this comes at a cost: Ruby is dynamically typed, and so type errors in a Rails application can remain latent until run time, making debugging and maintenance harder. In this paper, we describe DRails, a novel tool that brings static typing to Rails applications to detect a range of run time errors. DRails works by translating Rails programs into pure Ruby code in which Rails’s numerous implicit conventions are made explicit. We then discover type errors by applying DRuby, a previously developed static type inference system, to the translated program. We ran DRails on a suite of applications and found that it was able to detect several previously unknown errors.",user_id: 1, :document => File.open(File.join(Rails.root, '/public/system/publications/pdf/stror.pdf')) )
 
 
 
 user = User.find_by_id(1)
 user.skills << Skill.find_by_id(1)
+user.skills << Skill.find_by_id(2)
+user.skills << Skill.find_by_id(9)
+
+user = User.find_by_id(2)
+user.skills << Skill.find_by_id(1)
+user.skills << Skill.find_by_id(8)
+
+question = Question.find_by_id(1)
+question.skills << Skill.find_by_id(1)
+
+question = Question.find_by_id(2)
+question.skills << Skill.find_by_id(2)
+
+question = Question.find_by_id(3)
+question.skills << Skill.find_by_id(9)
+question.skills << Skill.find_by_id(10)
+
+job = Job.find_by_id(1)
+job.skills << Skill.find_by_id(2)
+
+job = Job.find_by_id(2)
+job.skills << Skill.find_by_id(1)
+
+job = Job.find_by_id(3)
+job.skills << Skill.find_by_id(9)
+
+publication = Publication.find_by_id(1)
+publication.skills << Skill.find_by_id(8)
+
+publication = Publication.find_by_id(2)
+publication.skills << Skill.find_by_id(1)
