8b0cfe64b64b17d8d6b316f89d2a359f121d11ea
Nuno Alves <nr.alves@campus.fct.unl.pt>
2014-12-10 02:36:01 +0000

    Varios detalhes limados

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -97,6 +97,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :body, :author, :available, :user_id)
+      params.require(:job).permit(:title, :body, :author, :available, :user_id, { skill_ids:[] })
     end
 end
